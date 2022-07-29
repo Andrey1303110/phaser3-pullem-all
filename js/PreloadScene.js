74127d163668cd199a403996bb10ca2fb35e86d0
andrey1303110 <andrey1303110@gmail.com>
2022-07-29 20:19:33 +0300

    add new images

--- js/PreloadScene.js
+++ js/PreloadScene.js
@@ -5,12 +5,14 @@ class PreloadScene extends Phaser.Scene {
 
     preload() {
         this.load.image('forest', 'sprites/forest.png');
+        this.load.image('rays', 'sprites/rays.png');
         this.load.image('stones', 'sprites/stones.png');
         this.load.image('sword', 'sprites/sword.png');
         this.load.image('cash', 'sprites/cash.png');
         this.load.image('income', 'sprites/income.png');
         this.load.image('stamina', 'sprites/stamina.png');
         this.load.image('strength', 'sprites/strength.png');
+        this.load.image('downloadButton', 'sprites/downloadButton.png');
         //this.load.audio('wings', 'assets/sounds/wings.mp3');
     }
 
